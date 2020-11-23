44bc7b10c7f136545c096dcca24de2afe1127e27
Aravind A <aravind.a@edureka.co>
2020-11-23 12:13:48 +0530

    pagination next prev btns added

--- src/components/clp/Project/Project.component.tsx
+++ src/components/clp/Project/Project.component.tsx
@@ -86,6 +86,40 @@ const IndustryProjects = () => {
       nextEl: `.swiper-button-next.${styles.next_btn}`,
       prevEl: `.swiper-button-prev.${styles.prev_btn}`,
     },
+    renderPrevButton: () => (
+      <div className={`swiper-button-prev ${styles.prev_btn}`}>
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          className="bi bi-arrow-left"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+          />
+        </svg>
+      </div>
+    ),
+    renderNextButton: () => (
+      <div className={`swiper-button-next ${styles.next_btn}`}>
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          className="bi bi-arrow-right"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+          />
+        </svg>
+      </div>
+    ),
     slidesPerView: 3,
     breakpoints: {
       1024: {
