9869484ce46bc6597230049669bd91e9f6a7c806
Aravind A <aravind.a@edureka.co>
2020-12-02 18:52:52 +0530

    nav css

--- src/components/shared/header/login_signout_nav/SignoutNav.component.tsx
+++ src/components/shared/header/login_signout_nav/SignoutNav.component.tsx
@@ -1,32 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { Nav,Button } from 'react-bootstrap';
-import styles from './loginsignoutnav.module.scss';
+import { Nav, Button } from "react-bootstrap";
+import styles from "./loginsignoutnav.module.scss";
 
 const SignoutNav = () => {
-  
-  return(
+  return (
     <>
-   
-    
-      
-      <Nav className={styles.right_links} >
-          <Nav.Item>
-            <Nav.Link className={styles.right_links_a} href="/">signout</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link className={styles.right_links_a} href="/">All Courses</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link className={styles.right_links_a} href="/">Blog</Nav.Link>
-          </Nav.Item>
-          <Button className={styles.login_in}>Log in</Button>
-          <Button className={styles.sign_up}>Sign up</Button>
+      <Nav className={styles.right_links}>
+        <Nav.Item>
+          <Nav.Link className={styles.right_links_a} href="/">
+            Corporate Training
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className={styles.right_links_a} href="/">
+            All Courses
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className={styles.right_links_a} href="/">
+            Blog
+          </Nav.Link>
+        </Nav.Item>
+        <Button className={styles.login_in}>Log in</Button>
+        <Button className={styles.sign_up}>Sign up</Button>
       </Nav>
-    
     </>
-    
-  )  
-   
-}
+  );
+};
 
 export default SignoutNav;
