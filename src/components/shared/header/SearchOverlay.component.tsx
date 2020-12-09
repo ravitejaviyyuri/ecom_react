f3e056658386689b6761add0fd76b8450c95e966
dharani <dharani.kumar@edureka.co>
2020-12-09 21:01:04 +0530

    search up to some extent added

--- src/components/shared/header/SearchOverlay.component.tsx
+++ src/components/shared/header/SearchOverlay.component.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import Modal from "react-bootstrap/Modal";
 import styles from "./searchoverlay.module.scss";
 import SearchInput from "./search_tabs/SearchInput.component";
 import MenuLinks from "./search_tabs/SearchTabs.component";
 import { CrossIcon } from "../../shared/icons/crossicon";
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'algoliasearch/lite';
 
 const SearchOverlay = (props: any) => {
+
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchClient = algoliasearch('testing9DFZ2ODF33','0ae09568a97f143707a7158a5c6de789');
+  const index = searchClient.initIndex('test_courses');
+
+  useEffect( () => {
+
+    index.search(searchInput)
+    .then(({ hits }) => {
+      console.log(hits);
+    });  
+
+  },[searchInput])
+  
+
   return (
     <Modal
       centered
@@ -18,7 +36,7 @@ const SearchOverlay = (props: any) => {
         <div className={styles.close_btn} onClick={props.handleClose}>
           <CrossIcon color="#000000" />
         </div>
-        <SearchInput />
+        <SearchInput  setSearchInput= {setSearchInput}/>
         <MenuLinks />
       </Modal.Body>
     </Modal>
--- src/components/shared/header/search_tabs/SearchInput.component.tsx
+++ src/components/shared/header/search_tabs/SearchInput.component.tsx
@@ -3,12 +3,22 @@ import Container from "react-bootstrap/Container";
 import { SearchIcon } from "../../icons/searchicon";
 import styles from "./searchinput.module.scss";
 
+
+//const searchClient = algoliasearch('testing9DFZ2ODF33','0ae09568a97f143707a7158a5c6de789');
+
+
 const SearchInput = (props: any) => {
+  const onChangeHandler = (event: any) =>{
+     console.log(event.target.value);
+     props.setSearchInput(event.target.value);
+  }
   return (
     <div className={styles.search_bx}>
       <Container>
         <div className={styles.input_bx}>
           <input
+             name="search"
+             onChange={onChangeHandler} 
             className={styles.input}
             type="text"
             placeholder="Enter Course, Category or keyword"
