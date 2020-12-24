5fc145a90c1ca91329cbd4ad4a720c1f282db869
Pranab Gupta <pranab.gupta@edureka.co>
2020-12-24 19:09:25 +0530

    locale check

--- next.config.js
+++ next.config.js
@@ -3,12 +3,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   })
   
   module.exports = withBundleAnalyzer({})
-  module.exports = {
-  i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ['en-US', 'en-IN','fr'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'en-US',}
-  }
+  
