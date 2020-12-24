61b48ee9bc88170259eb992e41c1eeb55063ec0a
Pranab Gupta <pranab.gupta@edureka.co>
2020-12-24 11:58:24 +0530

    type fix

--- src/components/clp/Certification/SampleCertificateModal.component.tsx
+++ src/components/clp/Certification/SampleCertificateModal.component.tsx
@@ -72,7 +72,7 @@ const SampleCertitficateModal = (props: any) => {
                 className="position-relative"
               >
                 <Form.Label className={styles.label}>Phone Number</Form.Label>
-                <CountryCodeSelect className={styles.code_select} />
+                <CountryCodeSelect className={styles.code_select} options = "" />
                 <Form.Control
                   className={`${styles.input} ${styles.phone_input}`}
                   type="tel"
