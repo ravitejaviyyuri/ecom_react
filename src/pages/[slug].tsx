d8aee59c2eec0abd8f1d6f13b3a51ebd0e6cc67d
Pranab Gupta <pranab.gupta@edureka.co>
2020-12-29 14:29:22 +0530

    code restructuring

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -99,15 +99,15 @@ const CoursePage = ({ data, errors }: Props) => {
           getaGlimpse={data.course.course_sections.clp_get_a_glimpse}
           courseOverview={data.course.course_sections.clp_course_overview}
         /> 
-        {/* <BatchComponent
+        <BatchComponent
           batches={data.batches}
           price={data.course.course_price}
           currencies={data.currencies}
           countries={data.countries}
-        /> */}
-        <Curriculum
-          curicullumData={data.course.course_sections.clp_curriuculum_section}
         />
+        {/* <Curriculum
+          curicullumData={data.course.course_sections.clp_curriuculum_section}
+        /> */}
         <Projects course_section={data.course.course_sections.clp_project} /> 
         <Certification />
         <EdurekaAdvantage
