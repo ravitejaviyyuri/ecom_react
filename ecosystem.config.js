87183afcc261bf909ed171516502be085fd15a03
Ubuntu <ubuntu@ip-172-31-3-120.ec2.internal>
2021-07-13 14:48:54 +0000

    added changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,10 +9,10 @@ module.exports = {
       deploy : {
         production : {
           "user" : 'ubuntu',
-          "host" : '107.21.225.214',
-          "ref"  : 'origin/master',
-          "repo" : 'git@bitbucket.org:edureka/ecom_react.git',
-          "path" : '/home/ubuntu/pm2-apps/ecom-react',
+          "host" : 'localhost',
+          "ref"  : 'feature/duq_comp',
+          "repo" : 'git@github.com:ravitejaviyyuri/ecom_react.git',
+          "path" : '/home/ubuntu/ecom_react',
           "pre-deploy-local": '',
           "post-deploy" : 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --name ECOM-NEXT',
           "pre-setup": ''
